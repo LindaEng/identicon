a83eadf4c6e6818f689f843b4c981cdf1d48deb8
= <90lineng@gmail.com>
2023-05-17 15:10:51 -0400

    pixel_map function created, Identicon.Image module updated with pixek_map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,13 +5,41 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
   end
 
-  def build_grid(%Identicon.Image{hex: hex} = _image) do
-    hex
-    |> Enum.chunk_every(3)
-    |> Enum.map(&mirror_row/1)
-    |> Enum.filter(&(&1 != []))
+  def build_pixel_map(%Identicon.Image{ grid: grid } = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+    %Identicon.Image{ image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
+      |> Enum.filter(&(&1 != []))
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row([_a]), do: []
